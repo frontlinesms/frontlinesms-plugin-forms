5c5a874e23e81147a1355421781b28aa21cfcf2d
Morgan Belkadi <morgan@frontlinesms.com>
2010-12-03 16:04:00 +0100

    Ready to release 0.02.29

--- src/main/java/net/frontlinesms/plugins/forms/FormsPluginController.java
+++ src/main/java/net/frontlinesms/plugins/forms/FormsPluginController.java
@@ -16,7 +16,6 @@ import net.frontlinesms.plugins.BasePluginController;
 import net.frontlinesms.plugins.PluginController;
 import net.frontlinesms.plugins.PluginControllerProperties;
 import net.frontlinesms.plugins.PluginInitialisationException;
-import net.frontlinesms.plugins.PluginSettingsController;
 import net.frontlinesms.plugins.forms.data.FormHandlingException;
 import net.frontlinesms.plugins.forms.data.domain.Form;
 import net.frontlinesms.plugins.forms.data.domain.FormResponse;
@@ -130,7 +129,9 @@ public class FormsPluginController extends BasePluginController implements Incom
 	
 	/** @see net.frontlinesms.plugins.PluginController#deinit() */
 	public void deinit() {
-		this.frontlineController.removeIncomingMessageListener(this);
+		if (this.frontlineController != null) {
+			this.frontlineController.removeIncomingMessageListener(this);
+		}
 	}
 
 	/** @see BasePluginController#initThinletTab(UiGeneratorController)  */
